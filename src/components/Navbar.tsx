a1af5c21e2794870c534142107c89e92eb6d8844
Jesús Quintana <jesquiliche@hotmail.comm>
2024-03-08 18:30:04 +0100

    Añadiendo meún Usuario

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,36 +1,31 @@
-import { PacificoFont, titleFont } from "@/config/fonts";
-import { Tipo } from "@/interfaces/interfaces";
+import { PacificoFont } from "@/config/fonts";
 import { fetchPaises, fetchTipos } from "@/services/api";
 import Link from "next/link";
 import SearchForm from "./Busqueda";
 import CartLinkComponent from "./CartLinkComponent";
 import ButtonAuth from "./ButtonAuth";
 
-
 const Navbar = async () => {
   const paisesData = await fetchPaises();
-
   const paises = paisesData?.data;
-
 
   const tiposData = await fetchTipos();
   const tipos = tiposData.data;
-  
 
   return (
     <>
-      <nav className="bg-white border opacity-95  w-full shadow-md fixed z-50">
+      <nav className="bg-white border opacity-95 w-full shadow-md fixed z-50">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-1">
           <Link href="/" className="flex items-center">
-            <img src="/LOGO.png" className="h-24 mr-3" alt="Logo" />
+            <img src="/LOGO.png" className="h-20 mr-3" alt="Logo" />
             <span
-              className={`${PacificoFont.className} hidden lg:block  text-3xl font-semibold whitespace-nowrap`}
+              className={`${PacificoFont.className} hidden lg:block text-3xl font-semibold whitespace-nowrap`}
             >
               El rincón de la cerveza
             </span>
           </Link>
           <div>
-          <SearchForm />
+            <SearchForm />
           </div>
           <button
             data-collapse-toggle="navbar-dropdown"
@@ -49,9 +44,9 @@ const Navbar = async () => {
             >
               <path
                 stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M1 1h15M1 7h15M1 13h15"
               />
             </svg>
@@ -65,7 +60,7 @@ const Navbar = async () => {
                 <button
                   id="dropdownNavbarLink"
                   data-dropdown-toggle="dropdownNavbar"
-                  className="flex items-center justify-between w-full py-2 pl-2 pr-4  rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-yellow-400 md:p-0 md:w-auto"
+                  className="flex items-center justify-between w-full py-2 pl-2 pr-4 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-yellow-400 md:p-0 md:w-auto"
                 >
                   Pais{" "}
                   <svg
@@ -77,9 +72,9 @@ const Navbar = async () => {
                   >
                     <path
                       stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
                       d="m1 1 4 4 4-4"
                     />
                   </svg>
@@ -123,9 +118,9 @@ const Navbar = async () => {
                   >
                     <path
                       stroke="currentColor"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      stroke-width="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
                       d="m1 1 4 4 4-4"
                     />
                   </svg>
@@ -140,7 +135,7 @@ const Navbar = async () => {
                     aria-labelledby="dropdownLargeButton"
                   >
                     {tipos &&
-                      tipos.map((t: Tipo) => (
+                      tipos.map((t) => (
                         <li key={t.id}>
                           <Link
                             href={`/Tipos/${t.id}`}
@@ -155,20 +150,76 @@ const Navbar = async () => {
               </li>
 
               <li>
-                <Link
-                  href="#"
-                  className="block py-2 pl-2 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-yellow-400 md:p-0 "
+                <CartLinkComponent />
+              </li>
+              {/* Menú de usuario */}
+              <li>
+                <button
+                  id="dropdownUserMenu"
+                  data-dropdown-toggle="userMenu"
+                  className="flex items-center justify-between w-full py-2 pl-2 pr-4 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-yellow-400 md:p-0 md:w-auto"
                 >
-                  Contacto
-                </Link>
+                  {/*Usuario*/}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  <svg
+                    className="w-2.5 h-2.5 ml-2.5"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 10 6"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="m1 1 4 4 4-4"
+                    />
+                  </svg>
+                </button>
+                {/* Menú desplegable de usuario */}
+                <div
+                  id="userMenu"
+                  className="z-10 opacity-100 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44"
+                >
+                  <ul
+                    className="border-1 rounded-lg shadow-md py-2 text-md text-gray-700"
+                    aria-labelledby="dropdownUserMenu"
+                  >
+                    <li className="text-md block px-3 text-dark rounded-es-md hover:text-white hover:bg-yellow-400">
+                      <ButtonAuth />
+                    </li>
+                    <li>
+                      <Link
+                        href="/configuracion"
+                        className="text-md block px-4 text-dark rounded-es-md hover:text-white hover:bg-yellow-400"
+                      >
+                        Configuración
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        href="/logout"
+                        className="text-md block px-4 text-dark rounded-es-md hover:text-white hover:bg-yellow-400"
+                      >
+                        Cerrar sesión
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
               </li>
-              <li>
-                <ButtonAuth/>
-              </li>
-             
-              <li>
-             <CartLinkComponent></CartLinkComponent>
-              </li>
+              {/* Fin del menú de usuario */}
             </ul>
           </div>
         </div>
