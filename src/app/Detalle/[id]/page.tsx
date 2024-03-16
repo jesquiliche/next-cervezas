25742906e535fa0b15f18451579bc89ff7365c40
Jesús Quintana <jesquiliche@hotmail.comm>
2024-03-16 15:10:59 +0100

    Responsive

--- src/app/Detalle/[id]/page.tsx
+++ src/app/Detalle/[id]/page.tsx
@@ -36,30 +36,32 @@ const CervezaHome: React.FC<Props> = async ({ params }) => {
 
   return (
     <div className="w-10/12 mx-auto py-32">
-      <div className="grid grid-cols-5 gap-4">
-        <div className="col-span-2">
+      <div className="grid grid-cols-1 md:grid-cols-5 gap-4">
+        <div className="col-span-1 md:col-span-2">
           <img
             src={cerveza.foto}
-            className="border-2 shadow-lg rounded-lg p-2"
+            className="border-2 shadow-lg rounded-lg p-2 w-full"
           />
         </div>
-        <div className="col-span-3">
-          <h1 className={`text-center text-4xl font-bold`}>{cerveza.nombre}</h1>
-          <div className="grid grid-cols-3 mt-12">
+        <div className="col-span-1 md:col-span-3">
+          <h1 className={`text-center text-xl md:text-4xl font-bold`}>
+            {cerveza.nombre}
+          </h1>
+          <div className="grid grid-cols-2 md:grid-cols-3 mt-12">
             <div className="col-span-1 mt-10">
-              <h2 className="text-xl font-bold">País</h2>
-              <h2 className="text-xl font-bold">Tipo</h2>
-              <h2 className="text-xl font-bold">Color</h2>
-              <h2 className="text-xl font-bold">Graduación</h2>
+              <h2 className="text-sm md:text-xl font-bold">País</h2>
+              <h2 className="text-sm md:text-xl font-bold">Tipo</h2>
+              <h2 className="text-sm md:text-xl font-bold">Color</h2>
+              <h2 className="text-sm md:text-xl font-bold">Graduación</h2>
             </div>
             <div className="col-span-1 mt-10">
-              <h2 className="text-xl">{cerveza.pais}</h2>
-              <h2 className="text-xl">{cerveza.tipo}</h2>
-              <h2 className="text-xl">{cerveza.color}</h2>
-              <h2 className="text-xl">{cerveza.graduacion}</h2>
+              <h2 className="text-sm md:text-xl">{cerveza.pais}</h2>
+              <h2 className="text-sm md:text-xl">{cerveza.tipo}</h2>
+              <h2 className="text-sm md:text-xl">{cerveza.color}</h2>
+              <h2 className="text-sm md:text-xl">{cerveza.graduacion}</h2>
             </div>
-            <div className="col-span-1">
-              <h2 className="text-5xl t font-bold text-center italic text-red-600 text-shadow-title">
+            <div className="col-span-2 md:col-span-1">
+              <h2 className="text-sm md:text-5xl t font-bold text-center italic text-red-600 text-shadow-title">
                 {cerveza.precio} €
               </h2>
               <AddCart cerveza={cerveza} />
@@ -68,8 +70,8 @@ const CervezaHome: React.FC<Props> = async ({ params }) => {
         </div>
 
         <div className="col-span-1 md:col-span-5">
-          <h1 className="text-font-bold text-4xl">Descripción</h1>
-          <p className={`text-lg mt-5`}>{cerveza.descripcion}</p>
+          <h1 className="text-font-bold text-sm md:text-4xl">Descripción</h1>
+          <p className={`text-sm md:text-lg mt-5`}>{cerveza.descripcion}</p>
         </div>
       </div>
     </div>
